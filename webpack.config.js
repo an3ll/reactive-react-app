2356fd23488d03cf828544514c794b94808496d6
an3ll <jocke.anell@gmail.com>
2018-03-07 15:53:30 +0100

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+const APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+const config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
+};
+
+module.exports = config;
