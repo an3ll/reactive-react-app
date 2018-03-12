d64c7c8c4f74e84855d2c8536cc6577fd5014e60
an3ll <jocke.anell@gmail.com>
2018-03-12 10:27:08 +0100

    refactoring

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import MessageInput from "./components/MessageInput";
+import BeerTable from "./components/BeerTable";
 
 class App extends Component {
+
     render () {
-        return <p>Hello React!</p>
+        return (
+            <div className='app row'>
+                <div className='col-xs-offset-3'>
+                    <BeerTable/>
+                    <MessageInput/>
+                </div>
+            </div>
+        )
     }
 }
-
 render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
     },
     resolve: {
         extensions: ['.js', '.jsx'],
-    }
+    },
 };
 
 module.exports = config;
